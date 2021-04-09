c60aa8d5fb72bbd946ae7672b77fcdb1d42393eb
wangxuetao <wangxuetao@192.168.0.101>
2021-04-09 22:57:03 +0800

    frist commit

---
+++ mvvm.js
@@ -0,0 +1,24 @@
+function hijack(data){
+    if( typeof data !=='object') return
+    for(let key of Object.keys(data)){
+        let val=data[key]
+        Object.defineProperty(data,key,{
+            enumerable:true,
+            configurable:false,
+            get(){
+                console.log('[hijack][get]->val:',val)
+                return val
+            },
+            set(newVal){
+                if(newVal===val) return
+                console.log('[hijack][set]->newVal:',newVal)
+                val=newVal
+
+                input.value=newVal
+                dispatchEvent.innerHTML=newVal
+                hijack(newVal)
+
+            }
+        })
+    }
+}
